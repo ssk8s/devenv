5d17883bd4075335f35f709ab43f5ed396d6c0c0
Jared Allard <jaredallard@users.noreply.github.com>
2021-06-16 14:29:28 -0700

    chore: classify devenv s3 bucket (#23)
    * chore: classify devenv s3 bucket
    
    * Delete .terraform.lock.hcl
    
    * Update main.tf

--- deployments/devenv/snapshots/main.tf
+++ deployments/devenv/snapshots/main.tf
@@ -1,8 +1,15 @@
 module "s3-testbucket" {
-  source                     = "git@github.com:getoutreach/terraform-modules.git//s3-datastorage?ref=c93f2d5"
-  owning_team                = "dev-tooling"
-  data_impact_classification = "Low"
-  bucket_name                = "outreach-devenv-snapshots"
+  source                = "git@github.com:getoutreach/terraform-modules.git//s3-datastorage?ref=246500c"
+  team                  = "cia-dev-tooling"
+  data_classification   = "High"
+  data_retention_months = "0"
+  personal_information  = "No"
+  environment           = "Development"
+  application           = "dev-environment"
+  bento                 = "None"
+  tier                  = "Non-essential"
+  k8s_cluster           = "None"
+  bucket_name           = "outreach-devenv-snapshots"
 }
 
 data "aws_iam_policy_document" "automated_snapshot_policy_data" {
