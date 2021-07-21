1fa208f22f241cef1e692599ae1eb36593020da9
Jared Allard <jared.allard@outreach.io>
2021-07-21 11:27:18 -0700

    fix(snapshot): skip-app -> skip-update

--- cmd/devenv/snapshot/snapshot_generate.go
+++ cmd/devenv/snapshot/snapshot_generate.go
@@ -256,7 +256,7 @@ func (o *Options) generateSnapshot(ctx context.Context, mc *minio.Client, s3c *s
 		o.log.Info("Deploying applications into devenv")
 		for _, app := range t.DeployApps {
 			o.log.WithField("application", app).Info("Deploying application")
-			cmd := exec.CommandContext(ctx, os.Args[0], "--skip-app", "deploy-app", app) //nolint:gosec
+			cmd := exec.CommandContext(ctx, os.Args[0], "--skip-update", "deploy-app", app) //nolint:gosec
 			cmd.Stderr = os.Stderr
 			cmd.Stdout = os.Stdout
 			cmd.Stdin = os.Stdin
