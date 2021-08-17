a4f9eafd0902bc84db283628f83d4aa0ed7cef23
George Shaw <george.e.shaw.iv@outlook.com>
2021-08-17 12:28:29 -0700

    feat: bump localizer version (#56)

--- pkg/kubernetestunnelruntime/localizer.go
+++ pkg/kubernetestunnelruntime/localizer.go
@@ -10,7 +10,7 @@ import (
 
 //nolint:gochecknoglobals
 var (
-	LocalizerVersion     = "v1.8.2"
+	LocalizerVersion     = "v1.12.0"
 	LocalizerDownloadURL = "https://github.com/getoutreach/localizer/releases/download/" +
 		LocalizerVersion + "/localizer_" + strings.TrimPrefix(LocalizerVersion, "v") + "_" +
 		runtime.GOOS + "_" + runtime.GOARCH + ".tar.gz"
